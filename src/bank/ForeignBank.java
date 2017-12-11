55e18e47cb5f76688ad2162725144a1b80d6b73d
ToastyTad <toastytadgit@gmail.com>
2017-12-11 15:18:34 -0500

    implemented alternative method

--- src/bank/ForeignBank.java
+++ src/bank/ForeignBank.java
@@ -3,8 +3,19 @@ package bank;
 import client.Client;
 
 public class ForeignBank implements Bank {
+    private int amountFromAccount;
+
     @Override
     public void performWithdrawal(Client client, int requiredAmount) {
+        amountFromAccount = 0;
+        if (client.isRewardsMember())
+            System.out.println("Sorry, we don't offer rewards member benefits at this location.");
+        amountFromAccount += 10;
+        notifyClient(requiredAmount);
+    }
 
+    private void notifyClient(int requiredAmount) {
+        System.out.println(String.format("You asked for: %s", requiredAmount));
+        System.out.println(String.format("Total withdrawn after fee: %s", amountFromAccount + requiredAmount));
     }
 }
--- src/bank/NativeCityBank.java
+++ src/bank/NativeCityBank.java
@@ -3,8 +3,24 @@ package bank;
 import client.Client;
 
 public class NativeCityBank implements Bank {
+    private int amountFromAccount;
+
     @Override
     public void performWithdrawal(Client client, int requiredAmount) {
+        amountFromAccount = 0;
+        if (client.isRewardsMember())
+            handleRewards();
+        amountFromAccount += 5;
+        notifyClient(requiredAmount);
+    }
 
+    private void handleRewards() {
+        System.out.println("We do have a rewards program! You saved $2!");
+        amountFromAccount -= 2;
+    }
+
+    private void notifyClient(int requiredAmount) {
+        System.out.println(String.format("You asked for: %s", requiredAmount));
+        System.out.println(String.format("Total withdrawn after fee: %s", amountFromAccount + requiredAmount));
     }
 }
